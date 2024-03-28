d2017ef3a7934ffae3eef7288241b787b86b1dd8
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-28 10:35:04 +0000

    DATE

---
+++ 01_basic/07_date.js
@@ -0,0 +1,31 @@
+let myDate =  new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString);
+//dd--mm--yyyy  month start from 1 otherwise from 0
+
+let  myCreateDate =  new Date(2023,0,23,5,3);
+
+
+let MyTimeStamp = Date.now()
+
+
+//to convert in second /1000
+
+
+MATH.floor(Date.now()/1000);
+
+
+let newDate = new Date()
+
+console.log(newData);
+newDate.getMonth+1;
+newDate.getDay;
+
+
+
+newDate.toLocaleDateString('default',{
+    weekday:"long",
+    
+})
