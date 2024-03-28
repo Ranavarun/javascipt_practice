31298c99d7c473caa08bb32b4bb177a602e6db87
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-28 09:27:12 +0000

    conversion and datA type

--- 01_basic/03_conversionDatatype.js
+++ 01_basic/03_conversionDatatype.js
@@ -1,29 +1,50 @@
-let score= "33abc";
-console.log(typeof score);
+// let score= "33abc";
+// console.log(typeof score);
 
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);//nan 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//nan 
 
-//value can be converted but as expected
-//"23"=>23
-//"24asb"=>nan
-//true=>1
-//false=>0
+// //value can be converted but as expected
+// //"23"=>23
+// //"24asb"=>nan
+// //true=>1
+// //false=>0
 
-let isLoggedIn = Boolean(1)
-console.log(isLoggedIn);//true
-//0=>false
-//"" =>false
-//"hites"=>true
-
-
-
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(someNumber);
+// let isLoggedIn = Boolean(1)
+// console.log(isLoggedIn);//true
+// //0=>false
+// //"" =>false
+// //"hites"=>true
 
 
 
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(someNumber);
+
+//********operator************
+
+
+// let value = 3
+// let  negValue  = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = "ji"
+
+// console.log(str2+str1);
+// console.log("1asad"+1);
+// console.log("1"+1+10);
+// console.log(1+2+"1");
+
+
+// let num1 , num2 ,num3
+
+// num1 = num2 = num3 = 2+2;
+
+
+let gameCounter = 100
+console.log(gameCounter++);
