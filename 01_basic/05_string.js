ee2adfd30874a7029480d955de7c0c24d49cb09f
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-28 09:54:20 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,33 @@
+const name = "kasa"
+const repocount = 50
+
+
+console.log(name+repoCount+"value");
+
+
+console.log(`hello my name is${name} and my repocunt is ${repocount}`);
+
+
+
+const names = new String('varrun')
+names__proto__
+//string method indexof charAt substring slice(negatively values) trim 
+
+
+
+const newstrings="    hi   ";
+console.log(newstrings.trim())0;//only for white space
+
+const url = "http://hitsh.com/hitets%20choudhary"
+
+
+console.log(url.replace('%20','-'))
+
+
+//split make string as array
+
+
+
+
+
+
