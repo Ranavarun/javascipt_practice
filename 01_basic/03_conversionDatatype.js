8b5f29fc6e8afe85ff2eec4e2218d76afa3d0e0d
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-28 08:52:38 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionDatatype.js
@@ -0,0 +1,29 @@
+let score= "33abc";
+console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//nan 
+
+//value can be converted but as expected
+//"23"=>23
+//"24asb"=>nan
+//true=>1
+//false=>0
+
+let isLoggedIn = Boolean(1)
+console.log(isLoggedIn);//true
+//0=>false
+//"" =>false
+//"hites"=>true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(someNumber);
+
+
+
