2359d718d4810acf9eb2c0b6056cfd67680656bb
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-28 10:21:45 +0000

    maths and random

---
+++ 01_basic/06_numandmaths.js
@@ -0,0 +1,33 @@
+// const  balanced = new Number(100)
+// balanced.toFixed(1)
+// balanced.toString.length
+
+
+// const otherNumber =123.999
+// otherNumber.toPrecision(2)
+
+
+// const hundred = 1000000
+// hundred.toLocaleString('en-IN')
+
+
+//MAX_VALUE MIN_VALUE
+
+
+//+++++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++
+// Math.abs(-1)
+// Math.round(4.6)
+// Math.ceil(4.6)
+// Math.floor(4.6)
+
+
+Math.random()
+
+(Math.random()*10)+1
+
+//min  max 
+
+const min  = 10
+const max =100
+
+Math.floor((Math.random()*(max-min+1))+min)
