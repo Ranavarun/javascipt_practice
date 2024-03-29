38b298057f20936baa0a623f40d1bf93b2d24223
Varun Rana <114384382+Ranavarun@users.noreply.github.com>
2024-03-29 09:31:06 +0000

    map

---
+++ 05_itratiion/07_seven.js
@@ -0,0 +1,16 @@
+//map is also caal backoption 
+//return value
+
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+
+//chaining sab ek sathe nahi hotha result pass hotha rahtha  ha
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
